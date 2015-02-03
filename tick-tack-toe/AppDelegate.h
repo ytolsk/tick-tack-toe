4c0cdbb48b2d8d43ce73d6fa3232bd79ee9b1d70
Daniel Shusta <pair+dshusta@pivotallabs.com>
2015-02-03 11:30:29 -0800

    Initial Commit

---
+++ tick-tack-toe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tick-tack-toe
+//
+//  Created by pivotal on 2/3/15.
+//  Copyright (c) 2015 blahm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tick-tack-toe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tick-tack-toe
+//
+//  Created by pivotal on 2/3/15.
+//  Copyright (c) 2015 blahm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
