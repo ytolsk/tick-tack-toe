75e218e70bed0a0ed9d3c40f1d203a970fd08da8
Eugenia Dellapenna and Yulia Tolskaya <pair+edellapenna+ytolskaya@pivotal.io>
2015-02-05 14:22:20 -0800

    Add Cedar

---
+++ tick-tack-toe Tests/ViewControllerSpec.mm
@@ -0,0 +1,17 @@
+#import <Cedar-iOS/Cedar-iOS.h>
+#import "ViewController.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(ViewControllerSpec)
+
+describe(@"ViewController", ^{
+    __block ViewController *controller;
+
+    beforeEach(^{
+        
+    });
+});
+
+SPEC_END
