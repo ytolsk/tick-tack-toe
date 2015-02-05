db3129e4ed655b28830e3173965f9e84de556bbd
Eugenia Dellapenna and Yulia Tolskaya <pair+edellapenna+ytolskaya@pivotal.io>
2015-02-05 14:51:37 -0800

    first tests

--- tick-tack-toe Tests/ViewControllerSpec.mm
+++ tick-tack-toe Tests/ViewControllerSpec.mm
@@ -1,5 +1,6 @@
 #import <Cedar-iOS/Cedar-iOS.h>
 #import "ViewController.h"
+#import "CollectionViewCell.h"
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
@@ -7,10 +8,27 @@ using namespace Cedar::Doubles;
 SPEC_BEGIN(ViewControllerSpec)
 
 describe(@"ViewController", ^{
-    __block ViewController *controller;
+    __block ViewController *controller; // _block flag needed so we can assign value to controller in a block
 
     beforeEach(^{
+        controller = [[ViewController alloc] init];
+        [controller view];  // this calls through to viewDidLoad (but only once, which is more realistic)
+    });
+    
+    describe(@"displays an empty game board", ^{
+        beforeEach(^{
+            [controller.collectionView layoutIfNeeded];
+        });
         
+        it(@"should have a collectionView with all empty cells", ^{
+            for (CollectionViewCell *cell in controller.collectionView.visibleCells) {
+                [cell empty] should equal(YES);
+            }
+        });
+        
+        it(@"should have nine cells", ^{
+            [controller.collectionView.visibleCells count] should equal(9);
+        });
     });
 });
 
